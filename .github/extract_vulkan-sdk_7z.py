7d5cfd57c77ea13474da1a6547fe71aa38be41a4
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-06-19 09:45:34 +0300

    ci: update vulkan sdk extraction script
    * use glslang 14.2.0 from vulkan sdk 1.3.283.0
    * fix hardcoded glslang and sdk versions
    * remove all files after glslang package creation
    * keep arm architecture in macos binaries

--- .github/extract_vulkan-sdk_7z.py
+++ .github/extract_vulkan-sdk_7z.py
@@ -14,30 +14,30 @@ f.write(content[pos7z:])
 
 '''
 
-VULKAN_SDK_VERSION=1.3.239.0
-GLSLANG_VERSION=12.0.0
+VULKAN_SDK_VERSION=1.3.283.0
+GLSLANG_VERSION=14.2.0
 
 # === Linux ===
-curl -JOL https://sdk.lunarg.com/sdk/download/1.3.239.0/linux/vulkansdk-linux-x86_64-$VULKAN_SDK_VERSION.tar.gz
+curl -JOL https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/linux/vulkansdk-linux-x86_64-$VULKAN_SDK_VERSION.tar.xz
 EXE_DIR=$VULKAN_SDK_VERSION/x86_64/bin
-tar --strip-components=3 -xf vulkansdk-linux-x86_64-$VULKAN_SDK_VERSION.tar.gz $EXE_DIR/glslangValidator $EXE_DIR/spirv-opt $EXE_DIR/spirv-remap
-tar -cJf glslang-12.0.0-Linux.tar.xz glslangValidator spirv-opt spirv-remap
-rm glslangValidator spirv-opt spirv-remap
+tar --strip-components=3 -xf vulkansdk-linux-x86_64-$VULKAN_SDK_VERSION.tar.xz $EXE_DIR/glslang $EXE_DIR/glslangValidator $EXE_DIR/spirv-opt $EXE_DIR/spirv-remap
+tar -cJf glslang-$GLSLANG_VERSION-Linux.tar.xz glslang glslangValidator spirv-opt spirv-remap
+rm vulkansdk-linux-x86_64-$VULKAN_SDK_VERSION.tar.xz glslang glslangValidator spirv-opt spirv-remap
 
 # === macOS ===
-curl -JOL https://sdk.lunarg.com/sdk/download/1.3.239.0/mac/vulkansdk-macos-$VULKAN_SDK_VERSION.dmg
+curl -JOL https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/mac/vulkansdk-macos-$VULKAN_SDK_VERSION.dmg
 hdiutil attach vulkansdk-macos-$VULKAN_SDK_VERSION.dmg
 ./extract_vulkan-sdk_7z.py /Volumes/vulkansdk-macos-$VULKAN_SDK_VERSION/InstallVulkan.app/Contents/Resources/installer.dat
-./7za e vulkan-sdk.7z macOS/bin/glslangValidator macOS/bin/spirv-opt macOS/bin/spirv-remap
-for exe in glslangValidator spirv-opt spirv-remap; do lipo $exe -thin x86_64 -output $exe; done
-tar -cJf glslang-12.0.0-macOS.tar.xz glslangValidator spirv-opt spirv-remap
-rm vulkan-sdk.7z glslangValidator spirv-opt spirv-remap
+hdiutil detach /Volumes/vulkansdk-macos-$VULKAN_SDK_VERSION
+./7zz e vulkan-sdk.7z macOS/bin/glslang macOS/bin/glslangValidator macOS/bin/spirv-opt macOS/bin/spirv-remap
+tar -cJf glslang-$GLSLANG_VERSION-macOS.tar.xz glslang glslangValidator spirv-opt spirv-remap
+rm vulkansdk-macos-$VULKAN_SDK_VERSION.dmg vulkan-sdk.7z glslang glslangValidator spirv-opt spirv-remap
 
 # === Windows ===
-curl -JOL https://sdk.lunarg.com/sdk/download/1.3.239.0/windows/VulkanSDK-$VULKAN_SDK_VERSION-Installer.exe
-./extract_vulkan-sdk_7z.py VulkanSDK-1.3.239.0-Installer.exe
-./7za e vulkan-sdk.7z Bin/glslangValidator.exe Bin/spirv-opt.exe Bin/spirv-remap.exe
-tar -cJf glslang-12.0.0-Windows.tar.xz glslangValidator.exe spirv-opt.exe spirv-remap.exe
-rm vulkan-sdk.7z glslangValidator.exe spirv-opt.exe spirv-remap.exe
+curl -JOL https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/windows/VulkanSDK-$VULKAN_SDK_VERSION-Installer.exe
+./extract_vulkan-sdk_7z.py VulkanSDK-$VULKAN_SDK_VERSION-Installer.exe
+./7zz e vulkan-sdk.7z Bin/glslangValidator.exe Bin/spirv-opt.exe Bin/spirv-remap.exe
+tar -cJf glslang-$GLSLANG_VERSION-Windows.tar.xz glslangValidator.exe spirv-opt.exe spirv-remap.exe
+rm VulkanSDK-$VULKAN_SDK_VERSION-Installer.exe vulkan-sdk.7z glslangValidator.exe spirv-opt.exe spirv-remap.exe
 
 '''
